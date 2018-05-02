b80386a6690beb820ff8cef5515ded082411e822
Wylie Conlon <wylieconlon@gmail.com>
2018-05-02 16:00:11 -0400

    Require script to be loaded after DOM for popcode validation

--- api.js
+++ api.js
@@ -1,153 +1,143 @@
 "use strict";
 
-// window.WIF = {
-//   addRoom: function (){},
-//   enterRoom: function (){},
-//   connectRooms: function (){},
-//   displayMessage: function (){},
-//   setInputHandler: function (){},
-//   currentLocation: null,
-// };
+var rooms = {};
+var roomConnections = {};
 
-// window.addEventListener('DOMContentLoaded', function() {
-  var rooms = {};
-  var roomConnections = {};
+var levelHeader = document.createElement('div');
+levelHeader.className = 'wif-header';
+document.body.appendChild(levelHeader);
 
-  var levelHeader = document.createElement('div');
-  levelHeader.className = 'wif-header';
-  document.body.appendChild(levelHeader);
+var messageLog = document.createElement('p');
+messageLog.className = 'wif-messages';
+document.body.appendChild(messageLog);
 
-  var messageLog = document.createElement('p');
-  messageLog.className = 'wif-messages';
-  document.body.appendChild(messageLog);
+var input = document.createElement('input');
+input.className = 'wif-input';
+document.body.appendChild(input);
 
-  var input = document.createElement('input');
-  input.className = 'wif-input';
-  document.body.appendChild(input);
+var button = document.createElement('button');
+button.className = 'wif-button';
+button.innerText = 'Go'
+document.body.appendChild(button);
 
-  var button = document.createElement('button');
-  button.className = 'wif-button';
-  button.innerText = 'Go'
-  document.body.appendChild(button);
+var currentLocation;
+var destinations = [];
+var inputCallback;
 
-  var currentLocation;
-  var destinations = [];
-  var inputCallback;
-
-  function updateHeader() {
-    var currentLocation = WIF.currentLocation;
-    var connections = 'None';
-    if (roomConnections[currentLocation].length) {
-      connections = roomConnections[currentLocation].join(', ');
-    }
-
-    levelHeader.innerHTML = '<p>Current Room: <em>' + currentLocation + '</em></p><p>Connected rooms: <em>' + connections + '</em></p>';
+function updateHeader() {
+  var currentLocation = WIF.currentLocation;
+  var connections = 'None';
+  if (roomConnections[currentLocation].length) {
+    connections = roomConnections[currentLocation].join(', ');
   }
 
-  function displayMessage(message) {
-    var el = document.createElement('p');
-    messageLog.appendChild(document.createTextNode(message));
+  levelHeader.innerHTML = '<p>Current Room: <em>' + currentLocation + '</em></p><p>Connected rooms: <em>' + connections + '</em></p>';
+}
+
+function displayMessage(message) {
+  var el = document.createElement('p');
+  el.innerText = message;
+  messageLog.appendChild(el);
+}
+
+function addRoom(name, callback) {
+  if (typeof name !== 'string') {
+    alert('The first parameter of addRoom is room name, a string: WIF.addRoom("Dungeon", function() {...})');
+    return;
+  } else if (typeof callback !== 'function') {
+    alert('The first parameter of addRoom is room name, a string: WIF.addRoom("Dungeon", function() {...})');
+    return;
+  } else if (rooms[name] !== undefined) {
+    alert('Each room must have a unique name');
+    return;
   }
 
-  function addRoom(name, callback) {
-    if (typeof name !== 'string') {
-      alert('The first parameter of addRoom is room name, a string: WIF.addRoom("Dungeon", function() {...})');
-      return;
-    } else if (typeof callback !== 'function') {
-      alert('The first parameter of addRoom is room name, a string: WIF.addRoom("Dungeon", function() {...})');
-      return;
-    } else if (rooms[name] !== undefined) {
-      alert('Each room must have a unique name');
-      return;
-    }
+  rooms[name] = callback;
+  roomConnections[name] = [];
+}
 
-    rooms[name] = callback;
-    roomConnections[name] = [];
-  }
+function hasConnection(name1, name2) {
+  var connectionList = roomConnections[name1];
 
-  function hasConnection(name1, name2) {
-    var connectionList = roomConnections[name1];
-
-    for (var i = 0; i < connectionList.length; i++) {
-      if (connectionList[i] === name2) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  function connectRooms(name1, name2) {
-    if (typeof name1 !== 'string' || typeof name2 !== 'string') {
-      alert('WIF.connectRooms(room1, room2) expects two room names as input');
-      return;
-    } else if (typeof rooms[name1] === 'undefined') {
-      alert('You are trying to connect room ' + name1 + ', which has not been added yet.');
-      return;
-    } else if (typeof rooms[name2] === 'undefined') {
-      alert('You are trying to connect room ' + name2 + ', which has not been added yet.');
-      return;
-    } else if (hasConnection(name1, name2)) {
-      alert('The connection between ' + name1 + ' and ' + name2 + ' already exists.');
-      return;
-    }
-
-    roomConnections[name1].push(name2);
-    roomConnections[name2].push(name1);
-  }
-
-  function enterRoom(name) {
-    if (typeof name !== 'string') {
-      alert('You must call enterRoom with a string name, like WIF.enterRoom("Dungeon");');
-      return;
-    } else if (typeof rooms[name] !== 'function') {
-      alert('The name ' + name + ' does not match any rooms. Try WIF.addRoom(name, roomHandler);');
-      return;
-    }
-
-    rooms[name]();
-    WIF.currentLocation = name;
-
-    updateHeader();
-  }
-
-  input.addEventListener('keydown', processKeys);
-  button.addEventListener('click', processInput);
-
-  function processInput(e) {
-    var text = input.value.toLowerCase();
-    var splitText = text.split(' ');
-
-    if (inputCallback) {
-      inputCallback(text, splitText);
-    }
-
-    updateHeader();
-
-    input.value = '';
-    input.focus();
-  }
-
-  function setInputHandler(handler) {
-    if (typeof handler !== 'function') {
-      alert('Input handler must be a function');
-    }
-
-    inputCallback = handler;
-  }
-
-  function processKeys(e) {
-    if (e.keyCode === 13) {
-      processInput();
+  for (var i = 0; i < connectionList.length; i++) {
+    if (connectionList[i] === name2) {
+      return true;
     }
   }
 
-  window.WIF = {
-    addRoom: addRoom,
-    enterRoom: enterRoom,
-    connectRooms: connectRooms,
-    displayMessage: displayMessage,
-    setInputHandler: setInputHandler,
-    currentLocation: null,
-  };
-// });
+  return false;
+}
+
+function connectRooms(name1, name2) {
+  if (typeof name1 !== 'string' || typeof name2 !== 'string') {
+    alert('WIF.connectRooms(room1, room2) expects two room names as input');
+    return;
+  } else if (typeof rooms[name1] === 'undefined') {
+    alert('You are trying to connect room ' + name1 + ', which has not been added yet.');
+    return;
+  } else if (typeof rooms[name2] === 'undefined') {
+    alert('You are trying to connect room ' + name2 + ', which has not been added yet.');
+    return;
+  } else if (hasConnection(name1, name2)) {
+    alert('The connection between ' + name1 + ' and ' + name2 + ' already exists.');
+    return;
+  }
+
+  roomConnections[name1].push(name2);
+  roomConnections[name2].push(name1);
+}
+
+function enterRoom(name) {
+  if (typeof name !== 'string') {
+    alert('You must call enterRoom with a string name, like WIF.enterRoom("Dungeon");');
+    return;
+  } else if (typeof rooms[name] !== 'function') {
+    alert('The name ' + name + ' does not match any rooms. Try WIF.addRoom(name, roomHandler);');
+    return;
+  }
+
+  rooms[name]();
+  WIF.currentLocation = name;
+
+  updateHeader();
+}
+
+input.addEventListener('keydown', processKeys);
+button.addEventListener('click', processInput);
+
+function processInput(e) {
+  var text = input.value.toLowerCase();
+  var splitText = text.split(' ');
+
+  if (inputCallback) {
+    inputCallback(text, splitText);
+  }
+
+  updateHeader();
+
+  input.value = '';
+  input.focus();
+}
+
+function setInputHandler(handler) {
+  if (typeof handler !== 'function') {
+    alert('Input handler must be a function');
+  }
+
+  inputCallback = handler;
+}
+
+function processKeys(e) {
+  if (e.keyCode === 13) {
+    processInput();
+  }
+}
+
+window.WIF = {
+  addRoom: addRoom,
+  enterRoom: enterRoom,
+  connectRooms: connectRooms,
+  displayMessage: displayMessage,
+  setInputHandler: setInputHandler,
+  currentLocation: null,
+};
